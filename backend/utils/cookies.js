8b544dbd21f58a1790982e3ed461dfcfe9ba9658
ShettyGagan <gaganshetty2222@gmail.com>
2026-06-05 12:05:53 +0530

    feat: add utility functions for setting and clearing authentication cookies

--- backend/utils/cookies.js
+++ backend/utils/cookies.js
@@ -1,12 +1,16 @@
 export const setAuthCookie = (res, token) => {
   res.cookie("token", token, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    secure: true,
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
 
 export const clearAuthCookie = (res) => {
-  res.clearCookie("token");
+  res.clearCookie("token", {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+  });
 };
